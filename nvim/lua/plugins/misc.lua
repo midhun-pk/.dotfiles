0f003eec9343cf78e1f90815807c697abd57a98a
midhun-pk <pkr.midhun@gmail.com>
2024-11-03 21:49:01 +0530

    First commit

---
+++ nvim/lua/plugins/misc.lua
@@ -0,0 +1,78 @@
+-- Standalone plugins with less than 10 lines of config go here
+return {
+  {
+    -- tmux & split window navigation
+    'christoomey/vim-tmux-navigator',
+  },
+  {
+    -- autoclose tags
+    'windwp/nvim-ts-autotag',
+  },
+  {
+    -- auto surround tags
+    'tpope/vim-surround',
+  },
+  {
+    -- detect tabstop and shiftwidth automatically
+    'tpope/vim-sleuth',
+  },
+  {
+    -- Powerful Git integration for Vim
+    'tpope/vim-fugitive',
+  },
+  {
+    -- GitHub integration for vim-fugitive
+    'tpope/vim-rhubarb',
+  },
+  {
+    -- Hints keybinds
+    'folke/which-key.nvim',
+    opts = {
+      -- win = {
+      --   border = {
+      --     { '┌', 'FloatBorder' },
+      --     { '─', 'FloatBorder' },
+      --     { '┐', 'FloatBorder' },
+      --     { '│', 'FloatBorder' },
+      --     { '┘', 'FloatBorder' },
+      --     { '─', 'FloatBorder' },
+      --     { '└', 'FloatBorder' },
+      --     { '│', 'FloatBorder' },
+      --   },
+      -- },
+    },
+  },
+  {
+    -- Autoclose parentheses, brackets, quotes, etc.
+    'windwp/nvim-autopairs',
+    event = 'InsertEnter',
+    config = true,
+    opts = {},
+  },
+  {
+    -- Highlight todo, notes, etc in comments
+    'folke/todo-comments.nvim',
+    event = 'VimEnter',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = { signs = false },
+  },
+  {
+    -- high-performance color highlighter
+    'norcalli/nvim-colorizer.lua',
+    config = function()
+      require('colorizer').setup()
+    end,
+  },
+  {
+    -- Rename variable
+    'smjonas/inc-rename.nvim',
+    dependencies = {
+      'stevearc/dressing.nvim',
+    },
+    config = function()
+      require('inc_rename').setup({
+        input_buffer_type = 'dressing',
+      })
+    end,
+  },
+}
