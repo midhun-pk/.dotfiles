2cbbfc317d9795cfe45f4b29b5afd706a9628c27
midhun-pk <pkr.midhun@gmail.com>
2024-11-04 09:52:25 +0530

    Added markdown and mrkdown-preview for nvim

--- nvim/lua/plugins/misc.lua
+++ nvim/lua/plugins/misc.lua
@@ -75,4 +75,22 @@ return {
       })
     end,
   },
+  {
+    -- render markdown
+    'MeanderingProgrammer/markdown.nvim',
+    main = "render-markdown",
+    opts = {},
+    name = 'render-markdown', -- Only needed if you have another plugin named markdown.nvim
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you use the mini.nvim suite
+  },
+  {
+    -- preview markdown in browser
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function()
+      vim.opt.rtp:prepend(vim.fn.stdpath('data') .. '/lazy/markdown-preview.nvim')
+      vim.fn["mkdp#util#install"]()
+    end,
+  },
 }
