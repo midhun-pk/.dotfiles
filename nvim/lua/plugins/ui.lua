decef38dc3d4514f6e6ca4bc3fd551d95d383b00
midhun <midhun.pk@fortanix.com>
2026-02-11 16:31:35 +0530

    Fixed noice bug

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -17,6 +17,28 @@ return {
         popupmenu = {
           enabled = true,
         },
+        lsp = {
+          progress = {
+            enabled = true,
+            view = "mini", -- Unobtrusive bottom-right corner
+            throttle = 1000 / 30,
+          },
+          -- BUG: noice's LSP signature/hover handling steals focus while typing in insert mode
+          -- Known issue: https://github.com/folke/noice.nvim/issues/1016
+          -- Disabling noice's LSP handlers; using native LSP + blink.cmp instead
+          signature = {
+            enabled = false,
+          },
+          hover = {
+            enabled = false,
+          },
+          -- Override markdown rendering so that **blink.cmp** and other plugins use **Treesitter**
+          override = {
+            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+            ["vim.lsp.util.stylize_markdown"] = true,
+            ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+          },
+        },
       })
     end,
   }
